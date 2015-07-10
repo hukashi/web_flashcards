f846454dea71d145014e6732875e7657c9a3fce8
Geovanna RC <gechro@outlook.com>
2015-07-10 16:44:08 -0400

    Add foreign key to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :decks
-  has_many :rounds
+  has_many :decks, foreign_key: :creator_id
+  has_many :rounds, foreign_key: :player_id
 
   validates :first_name, :last_name, :email, :password_hash, :username, presence: true
   validates :email, :username, uniqueness: true

da9b4cc07b05ad5b21016890cfafc7a29a2ff744
Britney Wright <blw06g@gmail.com>
2015-07-11 09:58:47 -0400

    separate question, answer and stats into separate routes to ensure game flow is correct

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,29 +10,49 @@ end
 get '/games/:id' do
   @round = Round.find(params[:id])
   if @round.completed == true
-    erb :'game/stats'
+    redirect :"games/#{params[:id]}/stats"
   elsif
     @round.guesses.count == 0
     redirect :"/games/#{@round.id}/cards/1"
   else
     last_played = @round.guesses.last.card_id
     deck_index = @round.cards.index(last_played)
-    redirect :"/game/stats/#{@round.id}/cards/#{deck_index + 1}"
+    redirect :"/games/#{@round.id}/cards/#{deck_index.to_i + 1}"
    end
 end
 
+get '/games/:id/stats' do
+   @round = Round.find(params[:id])
+  erb :'games/stats'
+end
+
 get '/games/:round_id/cards/:index'  do
-  @deck_index = params[:index].to_i - 1
+  @deck_index = params[:index]
   @round = Round.find(params[:round_id])
   @deck = @round.deck
-  @card = @round.cards[@deck_index]
+  @card = @round.cards[@deck_index.to_i - 1]
     erb :'games/card_question'
 end
 
-post '/games/:round_id/cards/:index' do
+get '/games/:round_id/cards/:id/answer' do
+  @round = Round.find(params[:round_id])
+  @card = @round.cards[params[:id].to_i - 1]
+  @deck = @round.deck
+  @guess = Guess.find_by(round_id: @round.id, card_id: @card.id)
+  erb :'games/card_answer'
+end
+
+post '/games/:round_id/cards/:id/answer' do
   @guess = Guess.new(params[:guess])
+  @round = Round.find(params[:round_id])
+  @card = @round.cards[params[:id].to_i - 1]
+  if params[:player_answer] == @card.answer 
+    @guess.correct = true
+  else
+    @guess.correct = false
+  end
   if @guess.save
-    erb :'games/card_answer'
+    redirect "/games/#{params[:round_id]}/cards/#{params[:id]}/answer"
   else
     erb :'games/card_question'
   end

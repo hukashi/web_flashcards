f89fd2d83ac2ef2c5aa86488fbcc00ebbfcfe848
Britney Wright <blw06g@gmail.com>
2015-07-11 13:38:42 -0400

    add methods for correct and incorrect guess totals to Round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,12 @@ class Round < ActiveRecord::Base
   belongs_to :player, class_name: 'User'
   has_many :guesses
   has_many :cards, through: :deck
+
+  def correct_guesses
+    self.guesses.where(correct: true).count
+  end
+
+  def incorrect_guesses
+    self.guesses.where(correct: false).count
+  end
 end

8a6a683594ac32cee54e66748026210938dbcac3
George Paul <gpaul.empyrean@gmail.com>
2015-07-11 20:14:20 -0400

    Completely reformatted specs to work with shoulda matchers

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,44 +1,8 @@
 require_relative "../spec_helper"
-describe "Deck" do
-  before(:all) do
-    DatabaseCleaner.start
 
-    @joe = User.create!(first_name: "Joe",
-                         last_name: "Smith",
-                         email: "test@example.com",
-                         username: "first_player",
-                         password: "peanut")
-
-
-    @deck = Deck.create(name: "Cool People", creator_id: @joe.id)
-
-    cards = [
-      ["On which side of the card do we find the question?", "front"],
-      ["On which side of the card do we find the answer?", "back"]
-    ]
-
-    cards.each do |card|
-      Card.create(question: card[0], answer: card[1], deck_id: @deck.id)
-    end
-
-    @cards = Card.all
-
-    @round = Round.create(player_id: @joe.id, deck_id: @deck.id)
-  end
-
-  after(:all) do
-    DatabaseCleaner.clean
-  end
-
-  it "has a creator" do
-    expect(@deck.creator).to eq @joe
-  end
-
-  it "has rounds" do
-    expect(@round.deck).to eq @deck
-  end
-
-  it "has cards" do
-    expect(@deck.cards.count).to eq 2
-  end
+describe Deck do
+  it { should belong_to(:creator).class_name('User')}
+  it { should have_many :cards }
+  it { should have_many :rounds }
+  it { should validate_presence_of :name }
 end
--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,44 +1,8 @@
 require_relative "../spec_helper"
-describe "Round" do
-  before(:all) do
-    DatabaseCleaner.start
 
-    @joe = User.create!(first_name: "Joe", 
-                         last_name: "Smith", 
-                         email: "test@example.com",
-                         username: "first_player",
-                         password: "peanut")
-  
-
-    @deck = Deck.create(name: "Cool People")
-
-    cards = [
-      ["On which side of the card do we find the question?", "front"],
-      ["On which side of the card do we find the answer?", "back"]
-    ]
-
-    cards.each do |card|
-      Card.create(question: card[0], answer: card[1], deck_id: @deck.id)
-    end
-    
-    @cards = Card.all 
-
-    @round = Round.create(player_id: @joe.id, deck_id: @deck.id)
-  end
-
-  after(:all) do
-    DatabaseCleaner.clean
-  end
-
-  it "has a player" do
-    expect(@round.player).to eq @joe
-  end
-
-  it "has a deck" do
-    expect(@round.deck).to eq @deck
-  end
-
-  it "has cards" do
-    expect(@round.cards.count).to eq 2
-  end
+describe Round do
+  it { should belong_to :deck }
+  it { should belong_to(:player).class_name('User') }
+  it { should have_many :guesses}
+  it { should have_many(:cards).through(:deck)}
 end
